6eb5f1a580eec76e39bb93489f7b8748d01acb7e
ricardocubillo <ricardo.cubillo789@outlook>
2023-08-28 14:31:33 -0600

    Setup the wifi and firebase

--- feeder-esp32/src/main.cpp
+++ feeder-esp32/src/main.cpp
@@ -1,18 +1,28 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include "wifi-configuration.h"
+#include "firebase-rdb.h"
+#include "food-container.h"
+
+const int HSRC04_TRIG = 18;
+const int HSRC04_ECHO = 19;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(HSRC04_ECHO, INPUT);
+
+  pinMode(HSRC04_TRIG, OUTPUT);
+
+  setup_wifi();
+  setup_rdb();
+
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  int obj_distance = 0;
+  obj_distance = get_obj_distance(HSRC04_TRIG, HSRC04_ECHO);
+  send_obj_distance(obj_distance);
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
