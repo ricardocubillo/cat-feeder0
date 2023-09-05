8e8e70282da6febac5bdfcc952936a3c43d7afa1
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-04 23:21:11 -0600

    Add time picker modal

--- FeederUI/components/HomeScreen.js
+++ FeederUI/components/HomeScreen.js
@@ -1,10 +1,13 @@
 import { View, Text } from "react-native";
+
 import ShowTime from "./ShowTime";
+import PickTime from "./PickTime";
 
 export default function HomeScreen() {
   return (
     <View>
       <ShowTime />
+      <PickTime />
     </View>
   );
 }
