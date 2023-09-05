dedfeb398992c88533c32cde2f3f99394c42c656
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-04 23:20:09 -0600

    Change items icons

--- FeederUI/components/ManualAlimentation.js
+++ FeederUI/components/ManualAlimentation.js
@@ -1,10 +1,11 @@
+import { FontAwesome5 } from "@expo/vector-icons";
 import { List, Switch } from "react-native-paper";
 
 export default function ManualAlimentation() {
   return (
     <List.Item
       title={"Enable manual feeding"}
-      left={(props) => <List.Icon {...props} icon="cat" />}
+      left={(props) => <FontAwesome5 {...props} name="cat" size={24} color="black" />}
       right={() => <Switch />}
     />
   );
