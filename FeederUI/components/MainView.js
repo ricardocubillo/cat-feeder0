25c27b86bd676346e0e393000501a8b66aae8971
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-06 12:26:45 -0600

    Remove the settings stack view

--- FeederUI/components/MainView.js
+++ FeederUI/components/MainView.js
@@ -1,8 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import SelectWeigh from "./SelectWeigh";
 import BottomTab from "./BottomTab";
-import SettingsScreen  from "./SettingsScreen";
 
 
 const Stack = createNativeStackNavigator();
@@ -15,8 +13,6 @@ export default function MainView() {
         component={BottomTab}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="Settings" component={SettingsScreen} />
-      <Stack.Screen name="SelectWeigh" component={SelectWeigh} />
     </Stack.Navigator>
   );
 }
