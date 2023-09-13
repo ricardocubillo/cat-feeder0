4c0534ce285bf534776b0c4588b58a6dcb1e9c9b
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-13 16:29:00 -0600

    Retrive container value from rdb

--- FeederUI/components/FoodContainer.js
+++ FeederUI/components/FoodContainer.js
@@ -1,14 +1,35 @@
+import { useState, useEffect } from "react";
 import { StyleSheet, Text } from "react-native";
-import { Card, ProgressBar } from "react-native-paper";
+import { Card, ProgressBar, MD3Colors } from "react-native-paper";
+import { getDatabase, ref, onValue  } from "firebase/database";
 
+import Firebase from "../firebase-configuration";
 
 export default function FoodContainer() {
+  const [containerValue, setContainerValue] = useState(0);
+
+  const rdb = getDatabase(Firebase);
+  const getRef = ref(rdb, "/food_container/component/hsrc04/value");
+
+  
+  useEffect(() => {
+
+    onValue(getRef, (snapshot) => {
+      const getValue = snapshot.val();
+      setContainerValue(getValue.toFixed(2) / 100);
+    })
+  }, []);
+
+
   return (
     <Card style={styles.card_view}>
       <Card.Title title="Food Container"/>
       <Card.Content style={styles.card_content_position}>
-        <ProgressBar style={styles.progress_view} animatedValue={0.5}/>
-        <Text>Container Quatity: 90%</Text>
+        <ProgressBar 
+          style={styles.progress_view} 
+          animatedValue={containerValue} 
+          color={containerValue <= 0.3 ? "#ba1a1a" : "#663399"} />
+        <Text>Container Quatity: {containerValue * 100}%</Text>
       </Card.Content>
     </Card>  
   );
