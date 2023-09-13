dd2208457c350702feea8e6b6dc93ef9f5f11f63
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-12 19:40:17 -0600

    Use View component instead safeview

--- FeederUI/components/HomeScreen.js
+++ FeederUI/components/HomeScreen.js
@@ -1,14 +1,14 @@
-import { View, SafeAreaView } from "react-native";
+import { View } from "react-native";
 
 import ShowTime from "./ShowTime";
 import PickTime from "./PickTime";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
+    <View>
       <ShowTime />
       <PickTime />
-    </SafeAreaView>
+    </View>
   );
 }
 
