61e5cdbd3f839a21915c96501e8fefa985f96dc8
ricardocubillo <134452249+ricardocubillo@users.noreply.github.com>
2023-09-15 15:23:15 -0600

    Update TimeCardItem.js

--- FeederUI/components/TimeCardItem.js
+++ FeederUI/components/TimeCardItem.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { FontAwesome5 } from '@expo/vector-icons';
 import { FAB } from "react-native-paper";
 import { Swipeable, GestureHandlerRootView } from "react-native-gesture-handler";
 import { getDatabase, set, ref } from "firebase/database";
-import _ from "lodash"
 
 import TimeCard from "./TimeCard";
 import Firebase from "../firebase-configuration";
