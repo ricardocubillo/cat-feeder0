ec166f2930f50e9eb2349c943e4718c65959036e
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-15 10:31:10 -0600

    Add the alarm funcionality

--- FeederUI/components/TimeCardItem.js
+++ FeederUI/components/TimeCardItem.js
@@ -1,12 +1,43 @@
+import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { FontAwesome5 } from '@expo/vector-icons';
 import { FAB } from "react-native-paper";
-import { Swipeable, GestureHandlerRootView, ScrollView } from "react-native-gesture-handler";
+import { Swipeable, GestureHandlerRootView } from "react-native-gesture-handler";
+import { getDatabse, set, ref} from "firebase/database";
 
 import TimeCard from "./TimeCard";
+import Firebase from "../firebase-configuration";
 
 
 export default function TimeCardItem({ task, enableTask, onDelete }) {
+  const [motor, setMotor] = useState(false);
+  const rdb = getDatabse(Firebase);
+  
+  const refreshTime = () => {
+    let date = new Date();
+    let current_date = date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: true
+    });
+
+    checkTime(current_date);
+  }
+
+  setInterval(() => refreshTime, 1000);
+
+  const checkTime = (time) => {
+    if (enableTask === true) {
+      if (task === time) {
+        setMotor(true);
+      }
+    }
+
+    set(ref(rdb, "/feeder/component/motor/dc/"), {
+      value: motor
+    });
+  }
+
   const renderRightActions = () => {
     return (
       <FAB
