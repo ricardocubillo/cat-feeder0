63091e58fa000ebb7a5d3827aea204739585095a
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-16 14:45:38 -0600

    Write the manual feeding on db

--- FeederUI/components/ManualAlimentation.js
+++ FeederUI/components/ManualAlimentation.js
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
-import { List, Switch } from "react-native-paper";
+import { List, Switch} from "react-native-paper";
+import { getDatabase, set, ref } from "firebase/database"
+
+import Firebase from "../firebase-configuration";
 
 export default function ManualAlimentation() {
+  const [motor, setMotor] = useState(false);
+  const rdb = getDatabase(Firebase);
+
+  const onMotor = () => {
+    setMotor(prevState => !prevState);
+  }
+  set(ref(rdb, "/feeder/manual-feeding/component/motor/dc/"), {
+    value: motor
+  });
+
   return (
     <List.Item
       title={"Enable manual feeding"}
-      left={(props) => <FontAwesome5 {...props} name="cat" size={24} color="black" />}
-      right={() => <Switch />}
+      left={(props) =>
+        <FontAwesome5
+          {...props}
+          name="cat"
+          size={24}
+          color="black"
+        />}
+      right={() =>
+        <Switch
+          value={motor}
+          onValueChange={onMotor}
+        />}
     />
   );
 }
