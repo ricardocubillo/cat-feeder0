b56eb1d647171ab29c00d547564a5361aba86a66
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-16 22:26:33 -0600

    Update main.cpp

--- feeder-esp32/src/main.cpp
+++ feeder-esp32/src/main.cpp
@@ -3,26 +3,61 @@
 #include "wifi-configuration.h"
 #include "firebase-rdb.h"
 #include "food-container.h"
+#include "food-scale.h"
+#include "motor-control.h"
 
 const int HSRC04_TRIG = 18;
 const int HSRC04_ECHO = 19;
+const int ENA = 27;
+const int IN1 = 32;
+const int IN2 = 33;
+const int HX711_DT = 16;
+const int HX711_SCK = 4;
+const int MOTOR_PINS [3] = {ENA, IN1, IN2};
+
+int obj_distance = 0;
+int food_scale_weight = 0;
+int selected_cat_weight = 0;
+int pwm_speed = 200;
+bool alarm_state = false;
+bool manual_state = false;
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(HSRC04_ECHO, INPUT);
 
   pinMode(HSRC04_TRIG, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
 
   setup_wifi();
   setup_rdb();
+  init_food_scale(HX711_DT, HX711_SCK);
 
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int obj_distance = 0;
   obj_distance = get_obj_distance(HSRC04_TRIG, HSRC04_ECHO);
   send_obj_distance(obj_distance);
 
+  food_scale_weight = read_food_scale();
+  send_food_weight(food_scale_weight);
+  selected_cat_weight = user_sel_weight();
+
+  alarm_state = alarm();
+  manual_state = manual();
+  Serial.print(manual_state);
+
+  if (alarm_state == HIGH && food_scale_weight <= selected_cat_weight) {
+    turn_on_motor(MOTOR_PINS, pwm_speed);
+  } 
+  else if (manual_state == HIGH && food_scale_weight <= selected_cat_weight) {
+    turn_on_motor(MOTOR_PINS, pwm_speed);
+  }
+  else {
+    turn_off_motor(MOTOR_PINS);
+  }
 }
