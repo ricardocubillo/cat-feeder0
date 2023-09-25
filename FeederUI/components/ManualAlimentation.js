e91b3a0cb0c1f2f48b7e9b11d9f3043b6408897a
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-09-24 20:13:05 -0600

    Disable the motor signal when the motor is in off

--- FeederUI/components/ManualAlimentation.js
+++ FeederUI/components/ManualAlimentation.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FontAwesome5 } from "@expo/vector-icons";
 import { List, Switch} from "react-native-paper";
-import { getDatabase, set, ref } from "firebase/database"
+import { getDatabase, set, ref, onValue } from "firebase/database"
 
 import Firebase from "../firebase-configuration";
 
 export default function ManualAlimentation() {
   const [motor, setMotor] = useState(false);
   const rdb = getDatabase(Firebase);
+
+  const manualFeedingState = getDatabase(Firebase);
+  const getManualState = ref(
+    manualFeedingState,
+    "/feeder/feeding-state/component/motor/dc/value"
+  );
+
+  useEffect(() => {
+    onValue(getManualState, (snapshot) => {
+      const motorState = snapshot.val();
+
+      if (motorState == true) {
+        setMotor(false);
+      }
+    });
+  }, []);
 
   const onMotor = () => {
     setMotor(prevState => !prevState);
