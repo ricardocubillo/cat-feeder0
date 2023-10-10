cb71ccf0c00580fde2677e023de6886929a2a66b
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-10-10 11:28:05 -0600

    Show the privous weight value in Weight Card

--- FeederUI/components/WeightScale.js
+++ FeederUI/components/WeightScale.js
@@ -1,12 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { StyleSheet, Text } from "react-native";
 import { Card } from "react-native-paper";
 import { getDatabase, onValue, ref } from "firebase/database";
 
 import Firebase from "../firebase-configuration";
+import usePreviousValue from "../previousValue";
 
 export default function WeightScale() {
   const [weightScale, setWeightScale] = useState(0);
+  const prevWeight = usePreviousValue(weightScale);
 
   const weightScaleRDB = getDatabase(Firebase);
   const weightScaleRef = ref(weightScaleRDB, "/cat-scale/read/hx711/value");
@@ -18,11 +20,15 @@ export default function WeightScale() {
     });
   }, []);
 
+  console.log("Previours weight value", prevWeight);
+
   return (
     <Card style={styles.cardView}>
       <Card.Title title="Cat's weight" />
       <Card.Content style={styles.cardContent}>
-        <Text style={styles.textView}>{weightScale}g</Text>
+        <Text style={styles.textView}>
+          {prevWeight == undefined ? "0": prevWeight}g
+        </Text>
       </Card.Content>
     </Card>
   );
