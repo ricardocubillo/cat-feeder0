1a1663af4fa6905368d45247cb8fc3a79fa67107
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-11-13 11:14:46 -0600

    Add timeout

--- feeder-esp32/include/cat-scale.h
+++ feeder-esp32/include/cat-scale.h
@@ -2,7 +2,7 @@
 #define _CAT_SCALE_
 
 void init_cat_scale(const int dt, const int sck);
-int read_cat_scale();
+int read_cat_weight();
 void send_cat_weight(int weight);
 
 #endif
--- feeder-esp32/src/cat-scale.cpp
+++ feeder-esp32/src/cat-scale.cpp
@@ -3,40 +3,47 @@
 
 #include "cat-scale.h"
 
-#define HOLDOFF_TIME 2000
-
 HX711 cat_scale;
 FirebaseData cat_weight;
+
+const float OFFSET = 50682624;
 
 void init_cat_scale(const int dt, const int sck) {
     float calibration_factor = -23.156;
 
     cat_scale.begin(dt, sck);
     cat_scale.set_scale(calibration_factor);
+    cat_scale.set_offset(OFFSET);
     cat_scale.tare();
 }
 
-int read_cat_scale() {
-    int read_weight = 0;
-    read_weight = (int)cat_scale.get_units(5);
+int read_cat_weight() {
+    int get_weight = 0;
 
-    if (read_weight < 50) {
-        read_weight = 0;
+    get_weight = (int)cat_scale.get_units(10);
+    cat_scale.power_down();
+    delay(500);
+    cat_scale.power_up();
+
+    if (get_weight < 50) {
+        get_weight = 0;
     }
 
-    // Returns the weights in Kg
-    return read_weight / 1000;
+    // Returns weight value in Kg
+    return get_weight / 1000;
 }
 
-void send_cat_weight(int weight) {
-    int prev_millis = 0;
+void send_cat_weight(int weight)
+{
+    int previous_millis = 0;
 
-    if (Firebase.ready() && (millis() - prev_millis > 1500 || prev_millis == 0)) {
-        prev_millis = millis();
+    if (Firebase.ready() && (millis() - previous_millis > 1500 || previous_millis == 0))
+    {
+        previous_millis = millis();
+
         Firebase.setInt(
-            cat_weight,
-            F("/cat-scale/read/hx711/value"),
-            weight
-        );
+            cat_weight, 
+            F("/cat-scale/read/hx711/value"), 
+            weight);
     }
 }
