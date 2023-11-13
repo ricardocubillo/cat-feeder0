6094be305cc4dc08aef660062f5128e77fb9d6fd
ricardocubillo <ricardo.cubillo789@outlook.com>
2023-11-13 11:14:57 -0600

    Add timeout

--- feeder-esp32/src/cat-scale.cpp
+++ feeder-esp32/src/cat-scale.cpp
@@ -8,7 +8,8 @@ FirebaseData cat_weight;
 
 const float OFFSET = 50682624;
 
-void init_cat_scale(const int dt, const int sck) {
+void init_cat_scale(const int dt, const int sck)
+{
     float calibration_factor = -23.156;
 
     cat_scale.begin(dt, sck);
@@ -17,15 +18,20 @@ void init_cat_scale(const int dt, const int sck) {
     cat_scale.tare();
 }
 
-int read_cat_weight() {
+int read_cat_weight()
+{
     int get_weight = 0;
 
-    get_weight = (int)cat_scale.get_units(10);
-    cat_scale.power_down();
-    delay(500);
-    cat_scale.power_up();
+    if (cat_scale.wait_ready_timeout(1000))
+    {
+        get_weight = (int)cat_scale.get_units(10);
+        cat_scale.power_down();
+        delay(500);
+        cat_scale.power_up();
+    }
 
-    if (get_weight < 50) {
+    if (get_weight < 50)
+    {
         get_weight = 0;
     }
 
@@ -42,8 +48,8 @@ void send_cat_weight(int weight)
         previous_millis = millis();
 
         Firebase.setInt(
-            cat_weight, 
-            F("/cat-scale/read/hx711/value"), 
+            cat_weight,
+            F("/cat-scale/read/hx711/value"),
             weight);
     }
 }
